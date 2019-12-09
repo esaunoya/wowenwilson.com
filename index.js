26d066046937868011df30c01ff9cab925a97b27
esaunoya <esaunoya@outlook.com>
2019-12-09 02:34:33 -0600

    wow it works

---
+++ index.js
@@ -0,0 +1,11 @@
+function sayWow(){
+    var ran = getRandom(22);
+    var path = 'wow/wow' + ran + '.mp3';
+
+    var audio = new Audio(path);
+    audio.play();
+}
+
+function getRandom(max) {
+    return Math.floor(Math.random() * 1000) % numElem;
+}
